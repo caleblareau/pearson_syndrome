cc640fa22d984da64ad407f5370ba83c30a7ad70
caleblareau <caleblareau@g.harvard.edu>
2020-08-03 03:09:08 -0400

    misc in vitro set up analyses

---
+++ invitro_ery_diff/code/01_deletion_changes.R
@@ -0,0 +1,24 @@
+library(data.table)
+library(dplyr)
+
+process_one_library <- function(lib, day){
+  cells <- fread(paste0("../output/cell_assignments_per_channel/Pearson_",lib,"_assign.tsv")) %>%
+    dplyr::filter(assign == "Pearson")
+  dels <- fread(paste0("../data/del_Pearson_Healthy_",lib,".deletion_heteroplasmy.tsv")) %>%
+    dplyr::filter(version == "improved" & deletion == "del10381-15407")
+  mdf <- merge(cells, dels, by.x = "barcode", by.y = "cell_id")
+  mdf$library <- lib
+  mdf$day <- day
+  
+  mdf
+}
+
+all_df <- rbind(
+  process_one_library("D6_1", "Day06"),
+  process_one_library("D6_2", "Day06"),
+  process_one_library("D12_1", "Day12"),
+  process_one_library("D12_2", "Day12")
+)
+
+ggplot(all_df, aes(x = day, y = depth, color = library)) + 
+  geom_violin()
