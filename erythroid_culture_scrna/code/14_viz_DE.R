4471e8536193ce53dd6637bea27db20862eccf56
caleblareau <clareau@stanford.edu>
2021-03-10 22:37:23 -0700

    last tweak

--- erythroid_culture_scrna/code/14_viz_DE.R
+++ erythroid_culture_scrna/code/14_viz_DE.R
@@ -9,8 +9,8 @@ metagenes <- c("SLC2A1", "SLC2A2", "SLC2A3", "SLC2A4","SLC2A5", "HK1", "HK2", "H
                "PFKM","PFKL", "FBP1", "FBP2", "ALDOA", "ALDOB", "ALDOC", "TPI1", "GAPDH", "GAPDHS", "PGK1", "PGK2", "PGAM1", "PGAM2",
                "ENO1", "ENO2", "ENO3", "PKM2", "PKLR", "LDHA", "LDHB", "DHC", "LDHAL6B", "PCK1", "GOT1", "MDH1", "MPC1", "MPC2", 
                "PC", "GOT2", "MDH2", "PDHA1", "PDHA2", "PDHB", "DLAT", "DLD", "PDHX")
-ery_mdf %>% dplyr::filter(Row.names %in% metagenes) %>% data.frame() %>% arrange(desc(FDR.x)) %>%
-  ggplot(aes(x = logFC.x , y = logFC.y)) + geom_point()
+ery_mdf %>% dplyr::filter(Row.names %in% metagenes) %>% data.frame() %>% arrange(desc(signedTstat.y))
+  ggplot(aes(x = signedTstat.x , y = signedTstat.y)) + geom_point()
 
 ery_mdf %>% dplyr::filter(logFC.x < -1.3 | logFC.y < -1.3)
 
