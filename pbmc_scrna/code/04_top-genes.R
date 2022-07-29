48296659fd0374101bbec910b213252f317ac370
caleblareau <clareau@stanford.edu>
2022-07-28 21:18:15 -0700

    misc

--- pbmc_scrna/code/04_top-genes.R
+++ pbmc_scrna/code/04_top-genes.R
@@ -24,8 +24,41 @@ melted_list %>% group_by(gene) %>%
   ) %>%
   arrange(desc(SZ)) %>% mutate(rank = 1:n()) -> Rank_summarize_zscore
 
-melted_list %>%
-  filter(gene %in% c("UQCRB"))
+# compute a bonferroni adjusted pvalue
+qnorm(0.01/15000)
+
+table((Rank_summarize_zscore$SZ) >10 )
+table((Rank_summarize_zscore$SZ) < -10 )
+
+p1 <- ggplot(Rank_summarize_zscore, aes(x = rank, y = SZ)) + 
+  geom_point(size = 0.5) + pretty_plot(fontsize = 8) + L_border() +
+  geom_hline(yintercept = 0, linetype = 2) +
+  labs(x = "Association rank", y = "Mean z-score")
+p1
+
+cowplot::ggsave2(p1, file = "../plots/go_zscore.pdf", width =1.62, height = 2)
+
+# Get scores
+oxphos_pathway <- c("SNORD138","MIR4691","COX17","MIR7113","ATP5PD","ATP5MG","UQCR11","COX4I1",
+                    "COX5B","COX6A1","COX6A2","COX6B1","COX6C","COX7A1","COX7A2","COX7B","COX7C",
+                    "COX8A","COX11","COX15","UQCRQ","DMAC2L","SLC25A4","SLC25A5","SLC25A6","UQCR10",
+                    "NDUFS7","MT-ATP6","MT-ATP8","MT-CO1","MT-CO2","MT-CO3","MT-CYB","MT-ND1",
+                    "MT-ND2","MT-ND3","MT-ND4","MT-ND4L","MT-ND5","MT ND6","NDUFA1","NDUFA2","NDUFA3",
+                    "NDUFA4","NDUFA5","NDUFA6","NDUFA7","NDUFA8","NDUFA9","NDUFA10","NDUFAB1","NDUFB1",
+                    "NDUFB2","NDUFB3","NDUFB4","NDUFB5","NDUFB6","NDUFB7","NDUFB8","NDUFB9","NDUFB10",
+                    "NDUFC1","NDUFC2","NDUFS1","NDUFS2","NDUFS3","NDUFV1","NDUFS4","NDUFS5","NDUFS6",
+                    "NDUFS8","NDUFV2","NDUFV3","ATP5F1A","ATP5F1B","ATP5F1C","ATP5F1D","ATP5F1E",
+                    "ATP5PB","ATP5MC1","ATP5MC2","ATP5MC3","ATP5ME","ATP5PF","ATP5PO","NDUFA12",
+                    "SCO1","SDHA","SDHB","SDHC","SDHD","SURF1","UCP1","UCP2","UCP3","UQCRB","UQCRC1",
+                    "UQCRC2","UQCRFS1","UQCRH","SLC25A14","COX7A2L","COX5A","ATP5IF1","SLC25A27","ATP5MF")
+
+
+Rank_summarize_zscore %>%
+  filter(gene %in% oxphos_pathway) %>% write.table()
+
+Rank_summarize_zscore %>%
+  filter(gene %in% c("FOS", "JUN", "JUNB", "FOSB")) %>% write.table()
+
 
 write.table(Rank_summarize_zscore[,c(1,2)], 
             file = "../output/pbmc-total-assoc.rnk",
--- pbmc_scrna/code/05_top_gene_heatmap.R
+++ pbmc_scrna/code/05_top_gene_heatmap.R
@@ -70,77 +70,34 @@ lapply(donors, function(d){
     vec <- rowSums(counts[,bc])
     cpm <- vec/sum(vec)*1000000
     data.frame(donor = d, celltype = ct,
-               GPR85 = cpm["GPR85"], 
-               CXCL14 = cpm["CXCL14"], PLEKHD1 = cpm["PLEKHD1"],
-               JUN = cpm["JUN"], FOS = cpm["FOS"],
-               CD69 = cpm["CD69"],
-               ACTB = cpm["ACTB"],
-               ANXA1 = cpm["ANXA1"],
-               EEF1A1 = cpm["EEF1A1"],
-               NFAM1 = cpm["NFAM1"],
+               UQCRB = cpm["UQCRB"], ATP5MG = cpm["ATP5MG"],
+               COX4I1 = cpm["COX4I1"], NDUFA4 = cpm["NDUFA4"],
                
-               CD52 = cpm["CD52"],
-               STAT3 = cpm["STAT3"],
-               RGS1 = cpm["RGS1"],
-               SLC2A3 = cpm["SLC2A3"],
-               ALDOA = cpm["ALDOA"],
+               CXCL14 = cpm["CXCL14"], GPR85 = cpm["GPR85"],
                LINC01641 = cpm["LINC01641"], C12orf54 = cpm["C12orf54"])
   }) %>% rbindlist() 
 })%>% rbindlist() %>% data.frame() -> ddf
 
-
-p_a <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(CD52 + 1))) +
+p_a <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(COX4I1 + 1))) +
   geom_tile() +
-  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(10,16), oob = scales::squish) +
+  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(7,13), oob = scales::squish) +
   scale_x_discrete(expand = c(0,0)) +
   scale_y_discrete(limits = rev(levels(as.factor(ddf$celltype))), expand = c(0,0)) +
   labs(x = "", y = "") + 
   pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
 p_a
-cowplot::ggsave2(p_a, file = "../plots/CD52_expression.pdf", width = 2, height = 2.3)
+cowplot::ggsave2(p_a, file = "../plots/COX4I1_expression.pdf", width = 2.5, height = 2.3)
 
-p_a <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(ACTB + 1))) +
+
+p_a <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(UQCRB + 1))) +
   geom_tile() +
-  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(10,16), oob = scales::squish) +
+  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(8,12), oob = scales::squish) +
   scale_x_discrete(expand = c(0,0)) +
   scale_y_discrete(limits = rev(levels(as.factor(ddf$celltype))), expand = c(0,0)) +
   labs(x = "", y = "") + 
   pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
 p_a
-cowplot::ggsave2(p_a, file = "../plots/ACTB_expression.pdf", width = 2, height = 2.3)
-
-p_a <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(NFAM1 + 1))) +
-  geom_tile() +
-  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(0,6), oob = scales::squish) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(limits = rev(levels(as.factor(ddf$celltype))), expand = c(0,0)) +
-  labs(x = "", y = "") + 
-  pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
-p_a
-cowplot::ggsave2(p_a, file = "../plots/NFAM1_expression.pdf", width = 1.7, height = 2.3)
-
-
-p_a <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(NFAM1 + 1))) +
-  geom_tile() +
-  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(0,6), oob = scales::squish) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(limits = rev(levels(as.factor(ddf$celltype))), expand = c(0,0)) +
-  labs(x = "", y = "") + 
-  pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
-p_a
-cowplot::ggsave2(p_a, file = "../plots/NFAM1_expression.pdf", width = 1.7, height = 2.3)
-
-
-p_cd69 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(CD69 + 1))) +
-  geom_tile() +
-  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(4,11), oob = scales::squish) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(limits = rev(levels(as.factor(ddf$celltype))), expand = c(0,0)) +
-  labs(x = "", y = "") + 
-  pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
-p_cd69
-
-cowplot::ggsave2(p_cd69, file = "../plots/CD69_expression.pdf", width = 1.7, height = 2.3)
+cowplot::ggsave2(p_a, file = "../plots/UQCRB_expression.pdf", width = 2.5, height = 2.3)
 
 
 p_gpr85 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(GPR85 + 1))) +
@@ -152,7 +109,7 @@ p_gpr85 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(GPR85 + 1))) +
   pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
 p_gpr85
 
-cowplot::ggsave2(p_gpr85, file = "../plots/GPR85_expression.pdf", width = 1.7, height = 2.3)
+cowplot::ggsave2(p_gpr85, file = "../plots/GPR85_expression.pdf", width = 2, height = 2.3)
 
 p_PLEKHD1 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(PLEKHD1 + 1))) +
   geom_tile() +
@@ -173,7 +130,6 @@ p_C12orf54 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(C12orf54 + 1)
 p_C12orf54
 cowplot::ggsave2(p_C12orf54, file = "../plots/C12orf54_expression.pdf", width = 1.7, height = 2.3)
 
-
 p_LINC01641 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(LINC01641 + 1))) +
   geom_tile() +
   scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(0,6), oob = scales::squish) +
@@ -182,8 +138,6 @@ p_LINC01641 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(LINC01641 + 
   labs(x = "", y = "") + 
   pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
 p_LINC01641
-
-cowplot::ggsave2(cowplot::plot_grid(p_C12orf54, p_LINC01641, p_PLEKHD1, nrow = 1), file = "../plots/supplment_expression.pdf", width = 5.1, height = 2.3)
 
 p_cxcl14 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(CXCL14 + 1))) +
   geom_tile() +
@@ -194,26 +148,10 @@ p_cxcl14 <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(CXCL14 + 1))) +
   pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
 p_cxcl14
 
-cowplot::ggsave2(p_cxcl14, file = "../plots/CXCL14_expression.pdf", width = 1.7, height = 2.3)
+
+cowplot::ggsave2(cowplot::plot_grid(p_C12orf54, p_LINC01641, p_cxcl14, nrow = 1), 
+                 file = "../plots/supplment_expression.pdf", width = 7, height = 2.3)
 
 
-pJun <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(JUN +1))) +
-  geom_tile() +
-  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(6,14), oob = scales::squish) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(limits = rev(levels(as.factor(ddf$celltype))), expand = c(0,0)) +
-  labs(x = "", y = "") + 
-  pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
-pJun
+cowplot::ggsave2(p_cxcl14, file = "../plots/CXCL14_expression.pdf", width = 2, height = 2.3)
 
-pFos <- ggplot(ddf, aes(x = donor, y = celltype, fill = log2(FOS +1))) +
-  geom_tile() +
-  scale_fill_gradientn(colors = jdb_palette("solar_rojos"), limits = c(6,14), oob = scales::squish) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(limits = rev(levels(as.factor(ddf$celltype))), expand = c(0,0)) +
-  labs(x = "", y = "") + 
-  pretty_plot(fontsize = 8) + L_border() + theme(legend.position = "none") 
-pFos
-
-cowplot::ggsave2(cowplot::plot_grid(pJun, pFos, nrow = 1),
-                 file = "../plots/JUNFOS_expression.pdf", width = 3.4, height = 2.3)
